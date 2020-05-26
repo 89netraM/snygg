7426c359414b27759a76419a5c1191a604bdd490
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-26 18:40:33 +0200

    Range

---
+++ showcase/range.js
@@ -0,0 +1,9 @@
+/** @type {HTMLInputElement} */
+const rangeInteractive = document.querySelector(`#range-interactive > input[type="range"]`);
+/** @type {HTMLSpanElement} */
+const rangeInteractiveDisplay = document.querySelector("#range-interactive > [data-range-value]");
+
+function updateDisplay() {
+	rangeInteractiveDisplay.setAttribute("data-range-value", rangeInteractive.valueAsNumber);
+}
+rangeInteractive.addEventListener("input", () => updateDisplay(), false);
