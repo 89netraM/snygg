9cb1a5f0e06db68bc70d786d0f4726f1ce0bcf6e
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-27 10:40:54 +0200

    Simpler encapsulated scripts

--- showcase/range.js
+++ showcase/range.js
@@ -1,9 +1,11 @@
-/** @type {HTMLInputElement} */
-const rangeInteractive = document.querySelector(`#range-interactive > input[type="range"]`);
-/** @type {HTMLSpanElement} */
-const rangeInteractiveDisplay = document.querySelector("#range-interactive > [data-range-value]");
+(() => {
+	/** @type {HTMLInputElement} */
+	const rangeInteractive = document.querySelector(`#range-interactive > input[type="range"]`);
+	/** @type {HTMLSpanElement} */
+	const rangeInteractiveDisplay = document.querySelector("#range-interactive > [data-range-value]");
 
-function updateDisplay() {
-	rangeInteractiveDisplay.setAttribute("data-range-value", rangeInteractive.valueAsNumber);
-}
-rangeInteractive.addEventListener("input", () => updateDisplay(), false);
+	function updateDisplay() {
+		rangeInteractiveDisplay.setAttribute("data-range-value", rangeInteractive.valueAsNumber);
+	}
+	rangeInteractive.addEventListener("input", () => updateDisplay(), false);
+})();
--- showcase/script.js
+++ showcase/script.js
@@ -1,73 +1,61 @@
-/** @type {Array<HTMLSelectElement>} */
-var sections;
+(() => {
+	/** @type {Array<HTMLSelectElement>} */
+	const sections = new Array(...document.querySelectorAll("section"));
+	/** @type {Array<HTMLLIElement>} */
+	const links = new Array(...document.querySelectorAll("nav > ul > li"));
+	/** @type {HTMLElement} */
+	const nav = document.querySelector("body > nav");
+	/** @type {HTMLElement} */
+	const more = document.querySelector("header > nav");
 
-/** @type {Array<HTMLLIElement>} */
-var links;
-
-/** @type {HTMLElement} */
-var nav;
-
-/** @type {HTMLElement} */
-var more;
-
-function hideAll() {
-	for (const section of sections) {
-		section.hidden = true;
+	function hideAll() {
+		for (const section of sections) {
+			section.hidden = true;
+		}
 	}
-}
 
-/** @param {string} id */
-function showOnly(id) {
-	hideAll();
-	sections.find(x => x.id === id).hidden = false;
-}
-
-function deactivateAll() {
-	for (const link of links) {
-		link.classList.remove("active");
+	/** @param {string} id */
+	function showOnly(id) {
+		hideAll();
+		sections.find(x => x.id === id).hidden = false;
 	}
-}
 
-/** @param {string} id */
-function activateOnly(id) {
-	deactivateAll();
-	links.find(x => x.querySelector(`[href="#${id}"]`) != null).classList.add("active");
-}
-
-/** @param {string} id */
-function select(id) {
-	showOnly(id);
-	activateOnly(id);
-}
-
-function updateByHash() {
-	select(location.hash.length > 0 ? location.hash.substring(1) : "home");
-	nav.classList.remove("visible");
-}
-
-/** @param {HTMLElement} pane */
-function togglePane(pane) {
-	if (pane.classList.contains("visible")) {
-		pane.classList.remove("visible");
+	function deactivateAll() {
+		for (const link of links) {
+			link.classList.remove("active");
+		}
 	}
-	else {
-		pane.classList.add("visible");
+
+	/** @param {string} id */
+	function activateOnly(id) {
+		deactivateAll();
+		links.find(x => x.querySelector(`[href="#${id}"]`) != null).classList.add("active");
 	}
-}
 
-window.addEventListener(
-	"load",
-	() => {
-		sections = new Array(...document.querySelectorAll("section"));
-		links = new Array(...document.querySelectorAll("nav > ul > li"));
-		nav = document.querySelector("body > nav");
-		more = document.querySelector("header > nav");
+	/** @param {string} id */
+	function select(id) {
+		showOnly(id);
+		activateOnly(id);
+	}
 
-		updateByHash();
-		document.querySelector("header > .expand-nav").addEventListener("click", () => togglePane(nav), false);
-		document.querySelector("header > .expand-more").addEventListener("click", () => togglePane(more), false);
-	},
-	false
-);
+	function updateByHash() {
+		select(location.hash.length > 0 ? location.hash.substring(1) : "home");
+		nav.classList.remove("visible");
+	}
 
-window.addEventListener("hashchange", () => updateByHash(), false);
+	/** @param {HTMLElement} pane */
+	function togglePane(pane) {
+		if (pane.classList.contains("visible")) {
+			pane.classList.remove("visible");
+		}
+		else {
+			pane.classList.add("visible");
+		}
+	}
+
+	updateByHash();
+	document.querySelector("header > .expand-nav").addEventListener("click", () => togglePane(nav), false);
+	document.querySelector("header > .expand-more").addEventListener("click", () => togglePane(more), false);
+
+	window.addEventListener("hashchange", () => updateByHash(), false);
+})();
